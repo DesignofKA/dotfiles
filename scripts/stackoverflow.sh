b64383806be7a90ae21fdf607d289be79606d9b2
kakposoe <kelvin.akposoe@gmail.com>
2019-04-05 17:01:49 +0100

    Changes

--- scripts/stackoverflow.sh
+++ scripts/stackoverflow.sh
@@ -1,4 +1,6 @@
-#! /bin/sh
-export DISPLAY=:0.0
-POINTS=$(http https://api.stackexchange.com/2.2/users/6449040\?order\=desc\&sort\=reputation\&site\=stackoverflow | jq '.items[0].reputation')
-su - $USER -c "/usr/bin/notify-send 'Stack Overflow Reputation' $POINTS"
+#! /bin/bash
+# PATH=$PATH:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+POINTS="$(/usr/bin/http https://api.stackexchange.com/2.2/users/6449040\?order\=desc\&sort\=reputation\&site\=stackoverflow)"
+# POINTS=$(echo ${POINTS} | /usr/bin/jq '.items[0].reputation')
+POINTS=$(echo ${POINTS} | /usr/bin/jq '.items[0].reputation')
+sudo -u kelvin DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send "Stack Overflow Reputation" $POINTS
